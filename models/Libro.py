47e8a93456f352ccd4fb61b80198061e717b9fe5
2dam <2dam@HZ341341.TartangaLH.eus>
2020-12-11 14:24:33 +0100

    Añadidos los atributos de Libro.

--- models/Libro.py
+++ models/Libro.py
@@ -7,12 +7,16 @@ from odoo import models
 class Libro(models.Model):
     _name = 'libros.libro'
     
-    idLibro = fields.Integer(string = "Id Libro", required = True) ###########
+    idLibro = fields.Integer(string = "Id Libro", required = True) ############### index
     titulo = fields.String(string = "Titulo", required = True)
     autor = fields.String(string = "Autor", required = True)
     editorial = fields.String(string = "Editorial", required = True)
     isbn = fields.Integer(string = "ISBN", required = True)
     genero = fields.String(string = "Genero literario", required = True)
+    cantidadTotal = fields.Integer(string = "Cantidad total", required = True)
+    cantidadDisponible = fields.Integer(string = "Cantidad disponible", required = True)
+    descargable = fields.Boolean(string = "Descargable", required = True)
+    linkDescarga = fields.String(string = "Link de descarga")
     
     
     
