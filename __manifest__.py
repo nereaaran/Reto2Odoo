7ec869bce45196de6daed5ef6af010a473b38fed
jony-ecua <jvc.ecua@gmail.com>
2021-01-11 14:02:06 +0100

    grupo view

--- __manifest__.py
+++ __manifest__.py
@@ -26,9 +26,11 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/view_grupo.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
+        
     ],
 }
